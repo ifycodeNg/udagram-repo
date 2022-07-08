33f0464c38c9c2eea8c35c01ebdfb994acf82942
ifycode <ifyugwumba22@gmail.com>
2022-07-08 15:05:18 +0100

    first commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export POSTGRES_USERNAME=admin_post
+export POSTGRES_PASSWORD=Password123
+export POSTGRES_HOST=dbifypost-1.c8tkiskwc3ao.eu-west-3.rds.amazonaws.com
+export POSTGRES_DB=apidb-dev
+export AWS_BUCKET=arn:aws:s3:::ifycode-3968-9780-9070-dev
+export AWS_REGION=eu-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
